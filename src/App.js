1cd1338ebf2a982a6fb717d812bef0af9fe06df2
Jory Oberlies-Rodrigues <oberliej@oregonstate.edu>
2023-01-22 12:20:37 -0800

    inital app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Time Tracker App</h1>
+      <p>
+        Velit sunt occaecat voluptate culpa excepteur Lorem velit do qui do
+        mollit ut. Duis pariatur cupidatat fugiat sunt quis eu qui. Eiusmod sunt
+        ut aliqua nisi deserunt commodo ex excepteur est dolor. Sit incididunt
+        ea reprehenderit velit et ea ullamco cillum fugiat. Deserunt culpa ad ex
+        culpa deserunt duis sit nostrud dolore. Cillum laboris laboris ipsum ut
+        cillum enim fugiat. Excepteur duis excepteur elit voluptate.
+      </p>
     </div>
   );
 }
